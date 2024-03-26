44e942dd630b83e6e25b41ef1e96bfeb54a05d8a
SAEED1799 <saeed.esawi99@gmail.com>
2024-03-26 14:07:33 +0200

    commit using jennkins

--- logic/home_page.py
+++ logic/home_page.py
@@ -1,7 +1,7 @@
 import time
 
-from selenium.webdriver import Keys
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
